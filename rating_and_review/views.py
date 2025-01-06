973dca0d971f89c3fcaa4e4b2f2d7a25789df46c
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-06 22:40:24 +0600

    update

--- rating_and_review/views.py
+++ rating_and_review/views.py
@@ -1,4 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets, views, permissions, generics,serializers
-
+from .models import RatingAndReviewModel
+from.serializers import RatingAndReviewSerializer
 # Create your views here.
+
+class RatingView(viewsets.ModelViewSet):
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    queryset=RatingAndReviewModel.objects.all()
+    serializer_class = RatingAndReviewSerializer
