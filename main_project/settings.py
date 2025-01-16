f097d739bb3943d3a63cae985bd3556cc02bf221
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-16 11:39:55 +0600

    fixed cors

--- main_project/settings.py
+++ main_project/settings.py
@@ -150,7 +150,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 CORS_ALLOW_ALL_ORIGINS = True
 CORS_ALLOWED_ORIGINS = [
-    'https://qrent.vercel.app/' # Replace with your frontend URL
+    'https://qrent.vercel.app' # Replace with your frontend URL
 ]
 CORS_ALLOW_METHODS = [
     'GET',
