89eb16b4cbd62e7780cbdda5d931824ede0320b9
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-17 19:07:05 +0600

    fixed login issue

--- account/serializers.py
+++ account/serializers.py
@@ -44,6 +44,6 @@ class RegistrationSerializer(serializers.ModelSerializer):
         return account
     
 
-class LoginSerializer(serializers.ModelSerializer):
+class LoginSerializer(serializers.Serializer):
     username=serializers.CharField(required=True)
     password=serializers.CharField(required=True)
