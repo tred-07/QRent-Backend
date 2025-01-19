bbccf8c669fe755864720f299a9267424bafb62f
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-01-19 18:43:54 +0600

    fixed AllAdvertise

--- advertise/views.py
+++ advertise/views.py
@@ -24,13 +24,13 @@ class AdvertiseListView(generics.ListAPIView):
         return super().get_queryset().filter(user=self.request.user)
     
 
-class AllAdvertise(viewsets.ListViewSet):
+class AllAdvertise(generics.ListAPIView):
     queryset = AdvertiseModel.objects.all()
     serializer_class = AdvertiseSerializer
     filter_backends=[DjangoFilterBackend,filters.SearchFilter,filters.OrderingFilter]
     filterset_fields=['user__username','title','description','price',]
     ordering_fields=['price','created_at','updated_at']
-        
+
 
 
 
