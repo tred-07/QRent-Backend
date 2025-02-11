c2b910a5eb284eed641d8a4c8c2e468e57ffe67c
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-02-11 21:08:00 +0600

    upd

--- request/serializers.py
+++ request/serializers.py
@@ -4,7 +4,7 @@ from rest_framework import serializers
 
 class RequestSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
-    advertise = serializers.StringRelatedField(read_only=True)
+    advertise = serializers.PrimaryKeyRelatedField(read_only=True)
     class Meta:
         model = RequestModel
         fields="__all__"
