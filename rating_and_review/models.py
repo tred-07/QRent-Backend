f22cee4bebb9e65b63aab0bd18fe670bf30db45b
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-02-11 11:31:57 +0600

    upd

--- rating_and_review/models.py
+++ rating_and_review/models.py
@@ -19,4 +19,4 @@ class RatingAndReviewModel(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return f"{self.user} {self.advertise} {self.star}"
+        return f"{self.user.first_name} {self.advertise} {self.star}"
