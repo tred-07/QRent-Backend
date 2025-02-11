64506dfc34f70013af0de9b64248e04a6d2e78dc
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-02-11 11:24:27 +0600

    upd

--- rating_and_review/admin.py
+++ rating_and_review/admin.py
@@ -2,7 +2,7 @@ from django.contrib import admin
 from .models import RatingAndReviewModel
 # Register your models here.
 class FeedbackAdmin(admin.ModelAdmin):
-    list_display = ['first_name','last_name','mobile_no']
+    list_display = ['first_name','last_name']
     def first_name(self,obj):
         return obj.user.first_name
     
