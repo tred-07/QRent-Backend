27b375e72cdc6fdc2a804e152a94c209f1df9ac4
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-02-14 00:53:51 +0600

    upd

--- favourite/urls.py
+++ favourite/urls.py
@@ -1,10 +1,12 @@
 from django.urls import include, path
 from rest_framework import routers
-from .views import FavouriteView
+from .views import FavouriteView,CreateFavourite
 
 router = routers.DefaultRouter()
-router.register('create', FavouriteView)
+
 
 urlpatterns = [
     path('', include(router.urls)),
+    path('/list',FavouriteView.as_view()),
+    path('create/<int:pk>',CreateFavourite.as_view())
 ]
--- favourite/views.py
+++ favourite/views.py
@@ -1,14 +1,31 @@
 from django.shortcuts import render
 from rest_framework import viewsets, views, permissions, generics,serializers
 from .models import FavouriteModel
+from advertise.models import AdvertiseModel
 from .serializers import FavouriteSerializer
 # Create your views here.
 
-class FavouriteView(viewsets.ModelViewSet):
+class FavouriteView(generics.ListAPIView):
     permission_classes = [permissions.IsAuthenticated]
     queryset = FavouriteModel.objects.all()
     serializer_class = FavouriteSerializer
     def get_queryset(self):
         return super().get_queryset().filter(user=self.request.user)
+
+
+class CreateFavourite(generics.CreateAPIView):
+    permission_classes=[permissions.IsAuthenticated]
+    serializer_class=FavouriteSerializer
+    # queryset=Review.objects.all()
+    def get_queryset(self):
+        pk=self.kwargs.get('pk')
+        return FavouriteModel.objects.filter(pk=pk)
     def perform_create(self,serializer):
-        serializer.save(user=self.request.user)
+        pk=self.kwargs.get('pk')
+        watchlist=AdvertiseModel.objects.get(pk=pk)
+        review_user=self.request.user
+        review_queryset=FavouriteModel.objects.filter(advertise=watchlist,user=review_user)
+        if review_queryset.exists():
+            raise serializers.ValidationError("You have already added.")
+        watchlist.save()
+        serializer.save(advertise=watchlist,user=review_user)
