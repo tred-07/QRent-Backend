bd3ff8b1b17975b41cba3a6296a4090e8d609bed
tred-07 <aranna.rtx.bd.02@gmail.com>
2025-02-14 00:15:44 +0600

    upd

--- favourite/serializers.py
+++ favourite/serializers.py
@@ -3,7 +3,7 @@ from .models import FavouriteModel
 
 class FavouriteSerializer(serializers.ModelSerializer):
     user=serializers.StringRelatedField(read_only=True)
-    advertise=serializers.StringRelatedField(read_only=True)
+    # advertise=serializers.StringRelatedField(read_only=True)
     class Meta:
         model = FavouriteModel
         fields = '__all__'
